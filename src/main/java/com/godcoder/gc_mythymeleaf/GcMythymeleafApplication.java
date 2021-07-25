292a071d79c72d40648c0943eeb4f86ab82659ba
SDC <sdcomms4227@gmail.com>
2021-07-25 22:35:32 +0900

    Initial commit

---
+++ src/main/java/com/godcoder/gc_mythymeleaf/GcMythymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.godcoder.gc_mythymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcMythymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcMythymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godcoder/gc_mythymeleaf/GcMythymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godcoder.gc_mythymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcMythymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
